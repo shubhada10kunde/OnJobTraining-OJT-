fa012d952b893eadcf863b918a04f73058516296
shubhada10kunde <shubhadakunde8@gmail.com>
2024-02-17 00:03:56 +0530

    some changes

--- StringMerge.cpp
+++ StringMerge.cpp
@@ -6,10 +6,10 @@ int main()
     string s2 = "abc";
     string answer;
     int i = 0, j = 0;
-    int StringSize1 = s1.size();
+    int StringSize1 = s1.size(); // will give the size of the string
     int StringSize2 = s2.size();
 
-    while (i < StringSize1 || j < StringSize2)
+    while (i < StringSize1 || j < StringSize2) // if 1st condition is true 2nd condition will not get exicute
     {
         if (i < StringSize1)
         {
