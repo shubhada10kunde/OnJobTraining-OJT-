dffde61a8d9602cb8aee16f243185d76d4e51b6b
shubhada10kunde <shubhadakunde8@gmail.com>
2024-03-04 23:57:43 +0530

    new file

---
+++ ValidParenthesis.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool isValid(string s)
+{
+    int sz = s.size();
+
+    stack<char> stk;
+    bool answer = true;
+    for (int i = 0; i < sz; i++)
+    {
+        if (s[i] == '{' || s[i] == '[' || s[i] == '(')
+        {
+            stk.push(s[i]);
+        }
+        else if (s[i] == ')')
+        {
+            if (stk.top() == '(')
+            {
+                stk.pop();
+            }
+            else
+            {
+                answer = false;
+                break;
+            }
+        }
+        else if (s[i] == ']')
+        {
+            if (stk.top() == '[')
+            {
+                stk.pop();
+            }
+            else
+            {
+                answer = false;
+                break;
+            }
+        }
+        else if (s[i] == '}')
+        {
+            if (stk.top() == '{')
+            {
+                stk.pop();
+            }
+            else
+            {
+                answer = false;
+                break;
+            }
+        }
+    }
+}
+
+int main()
+{
+    string s = "{([])}";
+
+    if (isValid(s))
+    {
+        cout << "Valid String" << endl;
+    }
+    else
+    {
+        cout << "Invalid string" << endl;
+    }
+}
