e6aeba903cdd5a9379bd133f9f9bf7afcde5906e
shubhada10kunde <shubhadakunde8@gmail.com>
2024-04-13 23:05:36 +0530

    No of Recent calls

---
+++ NumberOfRecentCalls.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class RecentCounter
+{
+public:
+    RecentCounter()
+    {
+    }
+
+    int ping(int t)
+    {
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
