545ad9fa6c7fc694c6e2b26e56aff2a996746faf
shubhada10kunde <shubhadakunde8@gmail.com>
2024-04-14 03:01:22 +0530

    no of recent calls

--- NumberOfRecentCalls.cpp
+++ NumberOfRecentCalls.cpp
@@ -4,14 +4,34 @@ using namespace std;
 class RecentCounter
 {
 public:
+    queue<int> MyQueue;
     RecentCounter()
     {
     }
 
     int ping(int t)
     {
+        MyQueue.push(t);
+
+        while (MyQueue.front() < t - 3000)
+        {
+            MyQueue.pop();
+        }
+        return MyQueue.size();
     }
 };
+
+int main()
+{
+    RecentCounter RC;
+
+    cout << RC.ping(1) << endl;
+    cout << RC.ping(100) << endl;
+    cout << RC.ping(3001) << endl;
+    cout << RC.ping(3002) << endl;
+
+    return 0;
+}
 
 /**
  * Your RecentCounter object will be instantiated and called as such:
